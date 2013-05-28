c92f9eb56316f566e71cfcc9d0aab8994d1768c7
sugamasao <sugamasao@gmail.com>
2013-05-29 01:58:02 +0900

    split of twitter class

--- lib/kanojo_bot.rb
+++ lib/kanojo_bot.rb
@@ -1,68 +1,9 @@
-# encoding: utf-8
+# coding: utf-8
 
 require 'logger'
-require 'tweetstream'
 
 require_relative 'text_processor'
-
-class TwitterWrapper
-  attr_reader :client, :stream
-
-  # @param [Logger] logger logger instance
-  def initialize(logger)
-    @logger = logger
-    @client = Twitter::Client.new(
-      consumer_key:       ENV['TWITTER_CONSUMER_KEY'],
-      consumer_secret:    ENV['TWITTER_CONSUMER_SECRET'],
-      oauth_token:        ENV['TWITTER_ACCESS_TOKEN'],
-      oauth_token_secret: ENV['TWITTER_ACCESS_TOKEN_SECRET'],
-    )
-    @profile = @client.verify_credentials
-
-    TweetStream.configure do |config|
-      config.consumer_key       = ENV['TWITTER_CONSUMER_KEY']
-      config.consumer_secret    = ENV['TWITTER_CONSUMER_SECRET']
-      config.oauth_token        = ENV['TWITTER_ACCESS_TOKEN']
-      config.oauth_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
-      config.auth_method        = :oauth
-    end
-
-    @stream = TweetStream::Client.new
-
-    @stream.on_error do |e|
-      @logger.error(e)
-    end
-
-    @stream.on_inited do
-      @logger.info('client init')
-    end
-  end
-
-  # user id is me?
-  #
-  # @param [Fixnum] id user id
-  def me?(id)
-    id == @profile.id
-  end
-
-  # tweet update! say daisuki
-  # @param [String] daisukidayo daisuki message 
-  # @param [Fixnum] id reply user id
-  def tweet_update(daisukidayo, id = nil)
-    option = {}
-    option[:in_reply_to_status_id] = id if id
-    @client.update(daisukidayo, option)
-  end
-
-  # exclude tweet
-  # @return [Boolean] true is exclude status
-  def exclude_tweet?(status)
-    return true if me?(status[:user][:id])
-    return true if status.retweet?
-    return true if status.reply?
-    return false
-  end
-end
+require_relative 'twitter_wrapper'
 
 class KanojoBot
 
@@ -79,7 +20,7 @@ class KanojoBot
   # running kanojo!
   def run
     @logger.debug 'run!'
-    @twitter.stream.userstream do |status|
+    @twitter.userstream do |status|
       @logger.debug(status)
       next if @twitter.exclude_tweet?(status)
 
